0eab24d930495e9f186bb7fc7e2db0a05fe6515d
Wanan531 <yucheng692@gmail.com>
2024-09-27 17:25:47 +0200

    Add files via upload

---
+++ projecto2792024/src/main/java/com/example/projecto2792024/Projecto2792024Application.java
@@ -0,0 +1,13 @@
+package com.example.projecto2792024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projecto2792024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projecto2792024Application.class, args);
+	}
+
+}
---
+++ projecto2792024/src/test/java/com/example/projecto2792024/Projecto2792024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecto2792024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projecto2792024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
